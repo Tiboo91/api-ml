2efc794f387d35adea38c7cb8fd707c159993abe
Tiboo91 <mekimedta@gmail.Com>
2022-11-21 19:41:27 +0100

    adding positive authentification test

--- source/test_main.py
+++ source/test_main.py
@@ -4,7 +4,19 @@ from .main import app
 
 client = TestClient(app)
 
-def test_read_main():
+def test_healthcheck():
     response = client.get("/healthCheck")
     assert response.status_code == 200
     assert response.json() == {"api_status": True}
+
+def test_openapi_schema():
+    response = client.get("/openapi.json")
+    assert response.status_code == 200
+
+def test_authentification():
+    response = client.post(
+        "/gettoken",
+        json={"username": "clementine", "password": "mandarine"},
+    )
+    assert response.status_code == 200
+
